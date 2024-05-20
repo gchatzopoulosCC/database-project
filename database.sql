727599b2a0ca3e1666c847f5ac2ba7000f1206ca
George Chatzopoulos <147397055+gchatzopoulosCC@users.noreply.github.com>
2024-05-20 20:49:24 +0300

    Add comments and improve the format

--- database.sql
+++ database.sql
@@ -1,3 +1,5 @@
+/* Product Related Tables */
+
 CREATE TABLE [Product] (
   [id] int PRIMARY KEY,
   [name] varchar(32) NOT NULL,
@@ -8,11 +10,15 @@ CREATE TABLE [Product] (
   [availability] tinyint DEFAULT (1),
   [seller_id] int
 );
+
+
 CREATE TABLE [OrderedProduct] (
   [id] int PRIMARY KEY,
   [product_id] int,
   [order_id] int
 );
+
+
 CREATE TABLE [Attributes] (
   [id] int PRIMARY KEY,
   [product_id] int UNIQUE,
@@ -29,6 +35,8 @@ CREATE TABLE [Attributes] (
   [storage_type] varchar(4),
   [operating_system] varchar(32)
 );
+
+
 CREATE TABLE [ProductRating] (
   [id] int PRIMARY KEY,
   [user_id] int,
@@ -37,6 +45,10 @@ CREATE TABLE [ProductRating] (
   [review] varchar(512),
   [experiences_json_path] varchar(256) UNIQUE NOT NULL
 );
+
+
+/* Seller Related */
+
 CREATE TABLE [Seller] (
   [id] int PRIMARY KEY,
   [name] varchar(32) UNIQUE NOT NULL,
@@ -49,11 +61,15 @@ CREATE TABLE [Seller] (
   [years_of_operation] int DEFAULT (0),
   [is_official_reseller] tinyint DEFAULT (0)
 );
+
+
 CREATE TABLE [SellerField] (
   [id] int PRIMARY KEY,
   [seller_id] int,
   [name] varchar(16) NOT NULL
 );
+
+
 CREATE TABLE [SellerReview] (
   [id] int PRIMARY KEY,
   [user_id] int,
@@ -61,10 +77,18 @@ CREATE TABLE [SellerReview] (
   [stars] int NOT NULL,
   [review] varchar(512)
 );
+
+
+/* Courier Related Tables */
+
 CREATE TABLE [Courier] (
   [id] int PRIMARY KEY,
   [name] varchar(32)
 );
+
+
+/* Order */
+
 CREATE TABLE [Order] (
   [id] int PRIMARY KEY,
   [buyer_id] int,
@@ -79,6 +103,10 @@ CREATE TABLE [Order] (
   [total_cost] float,
   [receipt_path] varchar(256) UNIQUE NOT NULL
 );
+
+
+/* User Related Tables */
+
 CREATE TABLE [User] (
   [id] int PRIMARY KEY,
   [username] varchar(32) NOT NULL,
@@ -87,28 +115,51 @@ CREATE TABLE [User] (
   [password_hash] varchar(64) NOT NULL,
   [phone_number] varchar(10) UNIQUE
 );
+
+
 CREATE TABLE [UserAddress] (
   [id] int PRIMARY KEY,
   [user_id] int,
   [address] varchar(128) UNIQUE NOT NULL
 );
+
+
+/* Favourite Products */
+
 CREATE TABLE [Favourite] (
   [id] int PRIMARY KEY,
   [user_id] int,
   [product_id] int
 );
 
+
+/* Foreign Keys*/
+
+/* Product */
+ALTER TABLE [Product] ADD FOREIGN KEY ([seller_id]) REFERENCES [Seller] ([id]);
+
+/* OrderedProduct */
 ALTER TABLE [OrderedProduct] ADD FOREIGN KEY ([product_id]) REFERENCES [Product] ([id]);
 ALTER TABLE [OrderedProduct] ADD FOREIGN KEY ([order_id]) REFERENCES [Order] ([id]);
+
+/* Attributes */
 ALTER TABLE [Attributes] ADD FOREIGN KEY ([product_id]) REFERENCES [Product] ([id]);
+
+/* ProductRating */
 ALTER TABLE [ProductRating] ADD FOREIGN KEY ([user_id]) REFERENCES [User] ([id]);
 ALTER TABLE [ProductRating] ADD FOREIGN KEY ([product_id]) REFERENCES [Product] ([id]);
-ALTER TABLE [SellerField] ADD FOREIGN KEY ([seller_id]) REFERENCES [Seller] ([id]);
+
+/* SellerReview */
 ALTER TABLE [SellerReview] ADD FOREIGN KEY ([user_id]) REFERENCES [User] ([id]);
 ALTER TABLE [SellerReview] ADD FOREIGN KEY ([seller_id]) REFERENCES [Seller] ([id]);
+
+/* Order */
 ALTER TABLE [Order] ADD FOREIGN KEY ([buyer_id]) REFERENCES [User] ([id]);
 ALTER TABLE [Order] ADD FOREIGN KEY ([seller_id]) REFERENCES [Seller] ([id]);
 ALTER TABLE [Order] ADD FOREIGN KEY ([courier_id]) REFERENCES [Courier] ([id]);
+
+/* UserAddress */
 ALTER TABLE [UserAddress] ADD FOREIGN KEY ([user_id]) REFERENCES [User] ([id]);
+
+/* Favourite */
 ALTER TABLE [Favourite] ADD FOREIGN KEY ([product_id]) REFERENCES [Product] ([id]);
-ALTER TABLE [Product] ADD FOREIGN KEY ([seller_id]) REFERENCES [Seller] ([id]);
